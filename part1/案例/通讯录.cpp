186ca9566a9c2d91884f55183a2f4a4fd3c604d2
huyanhai <810839700@qq.com>
2021-12-17 13:59:52 +0800

    feat: 通讯录

--- part1/案例/通讯录.cpp
+++ part1/案例/通讯录.cpp
@@ -18,9 +18,10 @@ struct contact
     int len;
 };
 
+// 操作菜单
 void show_menu()
 {
-    cout << "******************" << endl;
+    cout << "****************" << endl;
     cout << "*  1.添加联系人  *" << endl;
     cout << "*  2.显示联系人  *" << endl;
     cout << "*  3.删除联系人  *" << endl;
@@ -28,32 +29,33 @@ void show_menu()
     cout << "*  5.修改联系人  *" << endl;
     cout << "*  6.清空联系人  *" << endl;
     cout << "*  0.退出通讯录  *" << endl;
-    cout << "******************" << endl;
+    cout << "****************" << endl;
 }
 
-void add_contact(contact *contact)
+// 输出联系人
+void output_contact(contact *contact, int idx)
 {
-    if (contact->len > CONTACT_MAX)
-    {
-        cout << "通讯录已满" << endl;
-        return;
-    }
+    cout << "姓名是:" << contact->list[idx].name << ",性别是:" << contact->list[idx].sex << ",年龄是:" << contact->list[idx].age << ",电话号码是:" << contact->list[idx].tel << ",地址是:" << contact->list[idx].address << endl;
+}
 
+// 处理输入数据
+void input_contact(contact *contact, int idx)
+{
     string name = "";
     cout << "请输入姓名：" << endl;
     cin >> name;
-    contact->list[contact->len].name = name;
+    contact->list[idx].name = name;
 
     int sex;
     cout << "请输入性别：" << endl;
     cout << "1 - 男；2- 女" << endl;
-    cin >> sex;
 
     while (true)
     {
+        cin >> sex;
         if (sex == 1 || sex == 2)
         {
-            contact->list[contact->len].sex = sex;
+            contact->list[idx].sex = sex;
             break;
         }
         else
@@ -65,47 +67,126 @@ void add_contact(contact *contact)
     int age;
     cout << "请输入年龄：" << endl;
     cin >> age;
-    contact->list[contact->len].age = age;
+    contact->list[idx].age = age;
 
     string tel;
     cout << "请输入电话：" << endl;
     cin >> tel;
-    contact->list[contact->len].tel = tel;
+    contact->list[idx].tel = tel;
 
     string address;
     cout << "请输入地址：" << endl;
     cin >> address;
-    contact->list[contact->len].address = address;
+    contact->list[idx].address = address;
+}
 
+// 添加联系人
+void add_contact(contact *contact)
+{
+    if (contact->len > CONTACT_MAX)
+    {
+        cout << "通讯录已满" << endl;
+        return;
+    }
+    input_contact(contact, contact->len);
     contact->len++;
     cout << "联系人添加成功" << endl;
-
     return;
 }
 
+// 显示联系人
 void show_contact(contact *contact)
 {
+    if (contact->len < 1)
+    {
+        cout << "暂无联系人" << endl;
+        return;
+    }
     for (int i = 0; i < contact->len; i++)
     {
-        cout << "姓名是:" << contact->list[i].name << ",性别是:" << contact->list[i].sex << ",年龄是:" << contact->list[i].age << ",电话号码是:" << contact->list[i].tel << ",地址是:" << contact->list[i].address << endl;
+        output_contact(contact, i);
     }
-    return;
 }
 
-void del_contact(contact *contact)
+// 判断指定的联系人是否存在
+int find_contact_idx(contact *contact, string name)
 {
-    show_contact(contact);
-    cout << "请输入要删除的联系人姓名" << endl;
-    string name;
-    cin >> name;
     for (int i = 0; i < contact->len; i++)
     {
         if (contact->list[i].name == name)
         {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// 查找联系人
+void find_contact(contact *contact, int idx)
+{
+    output_contact(contact, idx);
+}
+
+// 删除指定联系人
+void del_contact(contact *contact, int idx)
+{
+    for (int i = idx; i < contact->len; i++)
+    {
+        contact->list[i] = contact->list[i + 1];
+    }
+    contact->len--;
+}
+
+// 编辑联系人
+void edit_contact(contact *contact, int idx)
+{
+    input_contact(contact, idx);
+    cout << "联系人修改成功" << endl;
+}
+
+// 清空通讯录
+void clear_contact(contact *contact)
+{
+    contact->len = 0;
+}
+
+void check_contact(contact *contact, int type)
+{
+    string tips = "编辑";
+    if (type == 1)
+    {
+        tips = "删除";
+    }
+    if (type == 2)
+    {
+        tips = "查找";
+    }
+
+    cout << "请输入要" << tips << "的人的姓名" << endl;
+    string name;
+    cin >> name;
+    int ret = find_contact_idx(contact, name);
+    if (ret > -1)
+    {
+        switch (type)
+        {
+        case 0:
+            edit_contact(contact, ret);
+            break;
+        case 1:
+            del_contact(contact, ret);
+            break;
+        case 2:
+            find_contact(contact, ret);
+            break;
+        default:
             break;
         }
     }
-    return;
+    else
+    {
+        cout << "通讯录中不存在" << name << endl;
+    }
 }
 
 int main()
@@ -126,13 +207,23 @@ int main()
             show_contact(&contact);
             break;
         case 3:
-            del_contact(&contact);
+        {
+            check_contact(&contact, 1);
             break;
+        }
         case 4:
+        {
+            check_contact(&contact, 2);
             break;
+        }
         case 5:
+        {
+            check_contact(&contact, 0);
             break;
+        }
+        break;
         case 6:
+            clear_contact(&contact);
             break;
         case 0:
             cout << "欢迎下次使用，系统将退出" << endl;
